3efb6991626c26d471ecde2d99d4d4eb30168a09
Maciej Szpindler <m.szpindler@uw.edu.pl>
2022-07-08 10:51:34 +0200

    Initial test for Abinit, untested and incomplete

---
+++ app-tests/abinit/abinit_check.py
@@ -0,0 +1,90 @@
+import reframe as rfm
+import reframe.utility.sanity as sn
+
+
+class abinit_check(rfm.RunOnlyRegressionTest):
+    modules = ['ABINIT-9.6.2']
+    executable = 'abinit'
+    maintainers = ['mszpindler']
+    strict_check = False
+
+    @run_after('init')
+    def set_input(self):
+        self.prerun_cmds = [
+                f'curl -LJO https://raw.githubusercontent.com/abinit/abinit/master/tests/paral/Input/t01.abi',
+                f'curl -LJO https://raw.githubusercontent.com/abinit/abinit/master/tests/Psps_for_tests/14si.psp'
+            ]
+        self.executable_opts = ['t01.abi']
+        self.variables = {
+                'ABI_PSPDIR': '.',
+        }
+
+    @run_after('init')
+    def set_prgenv(self):
+        if self.current_system.name in ['lumi']:
+            self.valid_prog_environs = ['cpeGNU']
+        else:
+            self.valid_prog_environs = ['builtin']
+
+    @sanity_function
+    def assert_simulation_success(self):
+        return sn.assert_found(r'Calculation completed',self.stdout)
+
+    @performance_function('s')
+    def time(self):
+        return sn.extractsingle(r'+Overall time at end (sec) : cpu=\s+\S+s+wall=\s+(?P<wtime>)',
+                                self.stdout, 'wtime', float)
+
+
+@rfm.simple_test
+class lumi_abinit_cpu_check(abinit_check):
+    scale = parameter(['small'])
+    valid_systems = ['lumi']
+    refs_by_scale = {
+        'small': {
+            'lumi:cpu': {'time': (152.644, None, 0.05, 's')}, # Fix it
+        },
+        'large': {
+            #
+        }
+    }
+
+    @run_after('init')
+    def setup_by_scale(self):
+        self.descr = f'Abinit CPU check (version: {self.scale})'
+        self.tags |= {'maintenance', 'production'}
+        if self.scale == 'small':
+            if self.current_system.name in ['lumi']:
+                self.num_tasks = 10
+
+        self.reference = self.refs_by_scale[self.scale]
+
+
+@rfm.simple_test
+class lumi_abinit_gpu_check(abinit_check):
+    # Fix it: GPU enabled Abinit instance (module)
+    scale = parameter(['small'])
+    valid_systems = ['lumi']
+    refs_by_scale = {
+        'small': {
+            'lumi:gpu': {'time': (182.0, None, 0.05, 's')}, # Fix it
+        },
+        'large': {
+            #
+        }
+    }
+
+    @run_after('init')
+    def setup_by_scale(self):
+        self.descr = f'Abinit GPU check (version: {self.scale})'
+        if self.scale == 'small':
+            if self.current_system.name in ['lumi']:
+                self.num_tasks = 24
+                self.num_tasks_per_node = 24
+                self.num_gpus_per_node = 1
+                #self.num_cpus_per_task = 2
+                #self.variables = {
+                #    'OMP_NUM_THREADS': str(self.num_cpus_per_task)
+                #}
+
+        self.reference = self.refs_by_scale[self.scale]
