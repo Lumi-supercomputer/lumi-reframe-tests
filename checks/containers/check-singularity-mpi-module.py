8c726313e432e0b8c56fec78ec9072b1cb0ef215
Rafael Sarmiento <rafael.sarmiento@cscs.ch>
2022-07-18 10:09:01 +0300

    add test for singularity mpich module

---
+++ checks/containers/check-singularity-mpi-module.py
@@ -0,0 +1,39 @@
+import os
+import reframe as rfm
+import reframe.utility.sanity as sn
+
+
+@rfm.simple_test
+class SingularityCrayMPICHBindingsTest(rfm.RunOnlyRegressionTest):
+    descr = 'Test the singularity-bindings module with glibc'
+    platform = parameter(['Singularity'])
+    valid_systems = ['lumi:gpu']
+    valid_prog_environs = ['builtin']
+    num_tasks = 2
+    num_tasks_per_node = 1
+    modules = ['singularity-bindings']
+    reference = {
+        'lumi:gpu': {'bandwidth': (22180.76, -0.05, None, 'MB/s')}
+    }
+
+    @run_before('run')
+    def prepare_build(self):
+        self.sourcesdir = os.path.join(self.current_system.resourcesdir,
+                                       'singularity-binding-test')
+
+    @run_before('run')
+    def set_container_variables(self):
+        self.descr = f'Run commands inside a container using {self.platform}'
+        self.container_platform = self.platform
+        self.container_platform.image = 'osu-debian-jessie.sif'
+        self.container_platform.command = '/home/osu/p2p_osu_bw'
+
+    @sanity_function
+    def assert_release(self):
+        max_bandwidth = r'4194304'
+        return sn.assert_found(max_bandwidth, self.stdout)
+
+    @performance_function('MB/s')
+    def bandwidth(self):
+        return sn.extractsingle(r'4194304\s+(?P<bw>\S+)',
+                                self.stdout, 'bw', float)
