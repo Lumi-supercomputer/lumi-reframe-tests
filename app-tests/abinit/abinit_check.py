1fc4a1e4eba29e0710eaf5f372c258e96e8047a7
Maciej Szpindler <m.szpindler@uw.edu.pl>
2022-07-22 10:08:25 +0200

    Increase value of nstep in Abinit input file to allow convergence

--- app-tests/abinit/abinit_check.py
+++ app-tests/abinit/abinit_check.py
@@ -11,8 +11,9 @@ class abinit_check(rfm.RunOnlyRegressionTest):
     @run_after('init')
     def set_input(self):
         self.prerun_cmds = [
-                f'curl -LJO https://raw.githubusercontent.com/abinit/abinit/master/tests/paral/Input/t01.abi',
-                f'curl -LJO https://raw.githubusercontent.com/abinit/abinit/master/tests/Psps_for_tests/14si.psp'
+                #f'curl -LJO https://raw.githubusercontent.com/abinit/abinit/master/tests/paral/Input/t01.abi',
+                f'sed -i -e "/nstep/s/2/20/" t01.abi',
+                #f'curl -LJO https://raw.githubusercontent.com/abinit/abinit/master/tests/Psps_for_tests/14si.psp'
             ]
         self.executable_opts = ['t01.abi']
         self.variables = {
@@ -32,7 +33,7 @@ class abinit_check(rfm.RunOnlyRegressionTest):
 
     @performance_function('s')
     def time(self):
-        return sn.extractsingle(r'+Overall time at end (sec) : cpu=\s+\S+s+wall=\s+(?P<wtime>)',
+        return sn.extractsingle(r'overall_wall_time:\s+(?P<wtime>\S+)'
                                 self.stdout, 'wtime', float)
 
 
@@ -42,7 +43,7 @@ class lumi_abinit_cpu_check(abinit_check):
     valid_systems = ['lumi']
     refs_by_scale = {
         'small': {
-            'lumi:cpu': {'time': (152.644, None, 0.05, 's')}, # Fix it
+            'lumi:cpu': {'time': (10.0, None, 5, 's')}, 
         },
         'large': {
             #
@@ -60,31 +61,31 @@ class lumi_abinit_cpu_check(abinit_check):
         self.reference = self.refs_by_scale[self.scale]
 
 
-@rfm.simple_test
-class lumi_abinit_gpu_check(abinit_check):
-    # Fix it: GPU enabled Abinit instance (module)
-    scale = parameter(['small'])
-    valid_systems = ['lumi']
-    refs_by_scale = {
-        'small': {
-            'lumi:gpu': {'time': (182.0, None, 0.05, 's')}, # Fix it
-        },
-        'large': {
-            #
-        }
-    }
-
-    @run_after('init')
-    def setup_by_scale(self):
-        self.descr = f'Abinit GPU check (version: {self.scale})'
-        if self.scale == 'small':
-            if self.current_system.name in ['lumi']:
-                self.num_tasks = 24
-                self.num_tasks_per_node = 24
-                self.num_gpus_per_node = 1
-                #self.num_cpus_per_task = 2
-                #self.variables = {
-                #    'OMP_NUM_THREADS': str(self.num_cpus_per_task)
-                #}
-
-        self.reference = self.refs_by_scale[self.scale]
+# Fix it: GPU enabled Abinit instance (module)
+#@rfm.simple_test
+#class lumi_abinit_gpu_check(abinit_check):
+#    scale = parameter(['small'])
+#    valid_systems = ['lumi']
+#    refs_by_scale = {
+#        'small': {
+#            'lumi:gpu': {'time': (182.0, None, 0.05, 's')}, # Fix it
+#        },
+#        'large': {
+#            #
+#        }
+#    }
+#
+#    @run_after('init')
+#    def setup_by_scale(self):
+#        self.descr = f'Abinit GPU check (version: {self.scale})'
+#        if self.scale == 'small':
+#            if self.current_system.name in ['lumi']:
+#                self.num_tasks = 24
+#                self.num_tasks_per_node = 24
+#                self.num_gpus_per_node = 1
+#                #self.num_cpus_per_task = 2
+#                #self.variables = {
+#                #    'OMP_NUM_THREADS': str(self.num_cpus_per_task)
+#                #}
+#
+#        self.reference = self.refs_by_scale[self.scale]
