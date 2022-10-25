142779d61834ead7bcf6741fbaa6d83da1d473ed
Rafael Sarmiento <rafael.sarmiento@cscs.ch>
2022-10-25 19:17:21 +0300

    add non-glibc version

--- checks/containers/check-singularity-mpi-module.py
+++ checks/containers/check-singularity-mpi-module.py
@@ -1,16 +1,16 @@
 import os
 import reframe as rfm
 import reframe.utility.sanity as sn
+from reframe.utility.osext import cray_cdt_version
 
 
-@rfm.simple_test
 class singularity_cray_mpich_bindings_test(rfm.RunOnlyRegressionTest):
     descr = 'Test the singularity-bindings module with glibc'
     valid_systems = ['lumi:gpu', 'lumi:standard']
     valid_prog_environs = ['builtin']
+    container_platform = 'Singularity'
     num_tasks = 2
     num_tasks_per_node = 1
-    modules = ['singularity-bindings']
     reference = {
         'lumi:gpu': {'bandwidth': (22180.76, -0.05, None, 'MB/s')}
     }
@@ -19,12 +19,6 @@ class singularity_cray_mpich_bindings_test(rfm.RunOnlyRegressionTest):
     def prepare_build(self):
         self.sourcesdir = os.path.join(self.current_system.resourcesdir,
                                        'singularity-binding-test')
-
-    @run_before('run')
-    def set_container_variables(self):
-        self.container_platform = 'Singularity'
-        self.container_platform.image = 'osu-debian-jessie.sif'
-        self.container_platform.command = '/home/osu/p2p_osu_bw'
 
     @sanity_function
     def assert_found_max_bandwidth(self):
@@ -35,3 +29,26 @@ class singularity_cray_mpich_bindings_test(rfm.RunOnlyRegressionTest):
     def bandwidth(self):
         return sn.extractsingle(r'4194304\s+(?P<bw>\S+)',
                                 self.stdout, 'bw', float)
+
+
+@rfm.simple_test
+class test_glibc_mount(singularity_cray_mpich_bindings_test):
+    modules = ['singularity-bindings']
+
+    @run_before('run')
+    def set_container_variables(self):
+        self.container_platform.image = 'osu-debian-jessie.sif'
+        self.container_platform.command = '/home/osu/p2p_osu_bw'
+
+
+@rfm.simple_test
+class test_no_glibc_mount(singularity_cray_mpich_bindings_test):
+    pe_version = cray_cdt_version()
+    modules = [f'singularity-bindings/system-cpeGNU-{pe_version}-noglibc']
+
+    @run_before('run')
+    def set_container_variables(self):
+        self.container_platform.image = 'osu-ubuntu-21.04.sif'
+        self.container_platform.command = (
+            '/usr/local/libexec/osu-micro-benchmarks/mpi/pt2pt/osu_bw'
+        )
