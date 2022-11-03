432ade16055f0006fa9b2001df0dfc2e230e8cff
Maciej Szpindler <56594900+mszpindler@users.noreply.github.com>
2022-11-03 10:40:43 +0100

    Update system-tests/compiler/openmp_gpu_offload.py
    Co-authored-by: rsarm <rafael.sarmiento@cscs.ch>

--- system-tests/compiler/openmp_gpu_offload.py
+++ system-tests/compiler/openmp_gpu_offload.py
@@ -9,7 +9,7 @@ class vAdd_ompGPU(rfm.RegressionTest):
     valid_prog_environs = ['cpeCray', 'cpeAOCC', 'builtin-hip']
     num_gpus_per_node = 1
     build_system = 'Make'
-    sourcesdir = 'https://code.ornl.gov/olcf/vector_addition'
+    sourcesdir = 'https://code.ornl.gov/olcf/vector_addition.git'
     prebuild_cmds = ['cd ompGPU/']
     executable = 'ompGPU/vAdd_ompGPU'
     variables = {'CRAY_ACC_DEBUG': '2', 'LD_LIBRARY_PATH': '$ROCM_PATH/hip/lib:$LD_LIBRARY_PATH'}
