4586e5dc6f56be3829c5c2bf8ed166f8e8310c52
Maciej Szpindler <m.szpindler@uw.edu.pl>
2023-11-06 17:31:36 +0200

    Update performance checking

--- checks/microbenchmarks/gpu/comm_scope.py
+++ checks/microbenchmarks/gpu/comm_scope.py
@@ -16,6 +16,15 @@ class comm_scope(rfm.RegressionTest):
     num_gpus_per_node = 8
     num_cpus_per_task = 8
 
+    # Reference numbers are taken from C. Pearson "Interconnect Bandwidth Heterogeneity on AMD MI250x and Infinity Fabric" (https://arxiv.org/pdf/2302.14827.pdf)
+    reference = {
+        'lumi:gpu': {
+            'quad':   (148, -0.1, 0.1, 'GB/s'),
+            'dual':   (76, -0.1, 0.1, 'GB/s'),
+            'single': (38, -0.1, 0.1, 'GB/s'),
+        }
+    }
+
     @run_after('setup')
     def setup_compile(self):
         self.build_job.num_cpus_per_task = 64
@@ -29,7 +38,8 @@ class comm_scope(rfm.RegressionTest):
     @run_before('compile')
     def do_cmake(self):
         self.prebuild_cmds = ['git submodule update --init --recursive']
-        self.build_system.config_opts = ['--fresh', '-DCMAKE_CXX_COMPILER=hipcc', '-DSCOPE_ARCH_MI250X=ON', '-DSCOPE_USE_NUMA=ON']
+        self.build_system.config_opts = ['--fresh', '-DCMAKE_CXX_COMPILER=hipcc', '-DSCOPE_ARCH_MI250X=ON', '-DSCOPE_USE_NUMA=ON', '-DCMAKE_CXX_FLAGS="-D__HIP_PLATFORM_AMD__"']
+        self.build_system.flags_from_environ = False
         self.build_system.builddir = 'build'
         self.build_system.max_concurrency = 8
 
@@ -38,5 +48,9 @@ class comm_scope(rfm.RegressionTest):
         return sn.assert_eq(sn.count( sn.findall('dst_gpu', 'rfm_job.json') ), self.num_gpus_per_node-1)
 
     @performance_function('B/s')
-    def bytes_per_second(self):
+    def bytes_per_second(self, kind='quad'):
+
+        if kind not in ('quad', 'dual', 'single'):
+            raise ValueError(f'illegal value in argument kind ({kind!r})')
+
         return sn.extractsingle(r'\s+.*"bytes_per_second"\:\s+(?P<bps>\S+),', 'rfm_job.json', 'bps', float)
