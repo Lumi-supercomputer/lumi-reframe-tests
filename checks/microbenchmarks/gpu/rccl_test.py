fff75786108abb2e11e39f167ff8aa97073f19dd
Maciej Szpindler <m.szpindler@cyfronet.pl>
2024-08-27 14:26:40 +0300

    Add rocm6 images and unify params

--- checks/microbenchmarks/gpu/rccl_test.py
+++ checks/microbenchmarks/gpu/rccl_test.py
@@ -15,7 +15,7 @@ class rccl_test_allreduce(rfm.RegressionTest):
     num_tasks = 16
     num_tasks_per_node = 8
     num_gpus_per_node = 8
-    executable_opts = ['-b 8', '-e 128M', '-f 2', '-g 1']
+    executable_opts = ['-b 2M', '-e 2048M', '-f 2', '-g 1', '-z 1', '-t 1', '-R 1', '-n 80', '-w 5', '-d half']
     executable = 'build/all_reduce_perf'
     exclusive_access = True
 
@@ -30,7 +30,7 @@ class rccl_test_allreduce(rfm.RegressionTest):
     def set_compiler_flags(self):
         self.sourcesdir = f'https://github.com/ROCmSoftwarePlatform/{self.repo_name}'
         self.build_system.builddir = 'build'
-        self.build_system.config_opts = ['--fresh', '-DMPI_MPICXX=CC', '-DCMAKE_CXX_COMPILER=hipcc', '-DCMAKE_CXX_FLAGS="--offload-arch=gfx90a --amdgpu-target=gfx90a --gcc-toolchain=$GCC_PATH/snos"','-DGPU_TARGETS=gfx90a', '-DMPI_PATH=$CRAY_MPICH_DIR', '-DCMAKE_EXE_LINKER_FLAGS="$PE_MPICH_GTL_DIR_amd_gfx90a -lmpi_gtl_hsa"']
+        self.build_system.config_opts = ['--fresh', '-DMPI_MPICXX=CC', '-DCMAKE_CXX_COMPILER=hipcc', '-DCMAKE_CXX_FLAGS="--offload-arch=gfx90a"','-DGPU_TARGETS=gfx90a', '-DMPI_PATH=$CRAY_MPICH_DIR', '-DCMAKE_EXE_LINKER_FLAGS="$PE_MPICH_GTL_DIR_amd_gfx90a -lmpi_gtl_hsa"']
         self.build_system.make_opts = ['VERBOSE=1', '-j8']
 
     @run_after('init')
@@ -39,10 +39,8 @@ class rccl_test_allreduce(rfm.RegressionTest):
             'NCCL_DEBUG': 'INFO',
             'NCCL_SOCKET_IFNAME': 'hsn0,hsn1,hsn2,hsn3',
             'NCCL_NET_GDR_LEVEL': '3',
+            'NCCL_ENABLE_DMABUF_SUPPORT': '1',
             'MPICH_GPU_SUPPORT_ENABLED': '1',
-            'CXI_FORK_SAFE': '1',
-            'CXI_FORK_SAFE_HP': '1',
-            'FI_CXI_DISABLE_CQ_HUGETLB': '1',
         }
     
     @run_before('run')
@@ -75,8 +73,8 @@ class rccl_test_allreduce_containerized(rfm.RunOnlyRegressionTest):
     num_tasks = 16
     num_tasks_per_node = 8
     num_gpus_per_node = 8
-    executable_opts = ['-b 8', '-e 128M', '-f 2', '-g 1']
     exclusive_access = True
+    cont_image = parameter(['6.0.3-dockerhash-1ada3e019646', '6.2.0-dockerhash-c8e37dff2e91', '6.1.3-dockerhash-4a063f050ed7'])
 
     reference = {
         'lumi:gpu': {
@@ -87,7 +85,7 @@ class rccl_test_allreduce_containerized(rfm.RunOnlyRegressionTest):
 
     @run_before('run')
     def set_launch_settings(self):
-        self.container_platform.image = '/appl/local/containers/sif-images/lumi-rocm-rocm-5.6.1.sif'
+        self.container_platform.image = f'/project/project_462000008/containers/lumi-rocm-rocm-{self.cont_image}.sif'
         self.container_platform.command = '/opt/rccltests/all_reduce_perf -z 1 -b 2M -e 2048M -f 2 -g 1 -t 1 -R 1 -n 80 -w 5 -d half'
         self.env_vars = {
             'NCCL_DEBUG': 'INFO',
