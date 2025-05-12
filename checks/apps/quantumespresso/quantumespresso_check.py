b897027fc9ea34ae42cac53aab890f1dcddbc27e
Philip P. Müller, LUMI <philip.mueller@cscs.ch>
2025-05-12 15:11:31 +0300

    Updated the tests again, now we have dedicated MPI and MPI+OMP tests.
    There are asome stuff I do not understand, but lets see what the others
    say.

--- checks/apps/quantumespresso/quantumespresso_check.py
+++ checks/apps/quantumespresso/quantumespresso_check.py
@@ -3,6 +3,7 @@ import reframe.utility.sanity as sn
 
 
 class quantumespresso_check(rfm.RunOnlyRegressionTest):
+    mode = parameter(['mpi', 'mpi_omp'])
     modules = ['QuantumESPRESSO']
     executable = 'pw.x'
     executable_opts += ['-in', 'ausurf.in', '-pd', '.true.']
@@ -35,19 +36,48 @@ class lumi_quantumespresso_cpu_check(quantumespresso_check):
     valid_systems = ['lumi:small']
     valid_prog_environs = ['cpeGNU']
     descr = f'QuantumESPRESSO CPU check'
-    num_tasks = 256
-    num_tasks_per_node = 128
-    num_cpus_per_task = 1
     time_limit = '15m'
 
-    # It seems that the reference value of the energy also depends on the node
-    #  configuration. The value selected below is the one used by CSCS in their
-    #  "large" configuration, which also has 256 nodes.
+    # It seems to be independent of the number of nodes/ranks.
     energy_reference = -11427.09017152
     energy_tolerance = 1.0e-6
 
-    # The time was obtained by running it multiple times and then compute
-    #  the average over these runs.
-    reference = {
-        'lumi:small': {'time': (56.16, None, 0.05, 's')},
-    } 
+    @run_after('init')
+    def setup_test(self):
+        self.descr = f'QuantumESPRESSO CPU check mode "{self.mode}"'
+
+        # In the CSCS test the option `num_tasks_per_core` is defined, however,
+        #  on LUMI this leads to a quite significant drop in performance.
+        #  Furthermore, in CSCS tests the variable `OMP_NUM_THREADS` is exported
+        #  but on LUMI this leads to a segfault.
+
+        if self.mode == 'mpi':
+            self.num_tasks = 256
+            self.num_tasks_per_node = 128
+            self.num_cpus_per_task = 1
+
+        else:
+            self.num_tasks = 128
+            self.num_tasks_per_node = 64
+            self.num_cpus_per_task = 2
+            self.env_vars = {
+                # If we define `OMP_NUM_THREADS` we get a segfault.
+                #'OMP_NUM_THREADS': self.num_cpus_per_task,
+            }
+
+
+    @run_before('performance')
+    def set_perf_reference(self):
+        references = {
+            'mpi': {
+                'lumi:small': {
+                    'time': (56.0, None, 0.05, 's')
+                },
+            },
+            'mpi_omp': {
+                'lumi:small': {
+                    'time': (60.00, None, 0.05, 's')
+                },
+            },
+        }
+        self.reference = references[self.mode]
