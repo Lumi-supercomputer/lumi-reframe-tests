32856d34ed2ef58835c1ebbb1d413c49094c648c
Maciej Szpindler <m.szpindler@uw.edu.pl>
2025-05-20 18:34:24 +0300

    Adjust performance numbers to one GCD

--- checks/apps/deeplearning/llm.c/src/hip/llmc/mfu.h
+++ checks/apps/deeplearning/llm.c/src/hip/llmc/mfu.h
@@ -44,7 +44,7 @@ static const PerfData AMPERE_DATACENTER = {156.f, 312.f, 312.f, 312.f, -1.f, -1.
 static const PerfData AMPERE_CONSUMER = {40.f, 80.f, 80.f, 160.f, -1.f, -1.f, 1860.f, 336.f};
 static const PerfData HOPPER = {378.f, 756.f, 756.f, 756.f, 1513.f, 1513.f, 1620.f, 456.f};
 static const PerfData ADA = {82.6f, 165.2f, 165.2f, 330.3f, 330.3f, 660.6f, 2520.f, 512.f};
-static const PerfData CDNA2 = {95.7f, 383.0f, 383.0f, -1.f, -1.f, -1.f, 1690.f, 208.f};
+static const PerfData CDNA2 = {95.7f, 191.5f, 191.5f, -1.f, -1.f, -1.f, 1600.f, 208.f};
 
 typedef struct {
     const char* name;
@@ -93,7 +93,7 @@ static GPUEntry gpu_db[] = {
     {"NVIDIA GeForce RTX 4060", &ADA, 96, 2460},
     {"NVIDIA H100 PCIe", &HOPPER, 456, 1620},
     {"NVIDIA H100 80GB HBM3", &HOPPER, 528, 1830}, // HBM3 = SXM5
-    {"AMD Instinct MI250X", &CDNA2, 208, 1690},
+    {"AMD Instinct MI250X", &CDNA2, 110, 1600},
 };
 
 float get_flops_promised(const char* device, int precision_mode) {
