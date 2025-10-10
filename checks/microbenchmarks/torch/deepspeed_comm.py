b3bba948fcd59e7bc8b3d9316a717aae71bf9df7
Maciej Szpindler <m.szpindler@uw.edu.pl>
2025-10-10 16:40:14 +0300

    Workaround aiter issue, add params

--- checks/microbenchmarks/torch/deepspeed_comm.py
+++ checks/microbenchmarks/torch/deepspeed_comm.py
@@ -10,9 +10,6 @@ class deepspeed_comm(rfm.RunOnlyRegressionTest):
     valid_prog_environs = ['builtin']
     sourcesdir = 'src'
     exclusive_access = True
-    num_tasks = 16
-    num_tasks_per_node = 8
-    num_gpus_per_node = 8
 
     perf_relative = variable(float, value=0.0, loggable=True)
 
@@ -24,16 +21,11 @@ class deepspeed_comm(rfm.RunOnlyRegressionTest):
         }
     }
 
-    @run_before('run')
-    def set_cpu_binding(self):
-        self.job.launcher.options = ['--cpu-bind="mask_cpu:0xfe000000000000,0xfe00000000000000,0xfe0000,0xfe000000,0xfe,0xfe00,0xfe00000000,0xfe0000000000"']
-
     @sanity_function
     def assert_job_is_complete(self):
         return sn.all([
-#            sn.assert_found(r'Using network AWS Libfabric', self.stdout),
-#            sn.assert_found(r'Selected Provider is cxi', self.stdout),
-            sn.assert_found(r'Performance of', self.stdout)  
+            sn.assert_found(r'Performance of', self.stdout),
+            sn.assert_eq(sn.count( sn.findall('x4', self.stdout) ), 23),
         ])
 
     #@performance_function('ms')
@@ -66,8 +58,15 @@ class deepspeed_comm(rfm.RunOnlyRegressionTest):
 class ds_comm_all_reduce(deepspeed_comm):
     modules = ['PyTorch/2.6.0-rocm-6.2.4-python-3.12-singularity-20250410']
     dist_mode = parameter(['deepspeed', 'torch'])
+    num_tasks = 16
+    num_tasks_per_node = 8
+    num_gpus_per_node = 8
 
-    tags = {'python', 'contrib'}
+    tags = {'python', 'contrib', 'performance'}
+
+    @run_before('run')
+    def set_cpu_binding(self):
+        self.job.launcher.options = ['--cpu-bind="mask_cpu:0xfe000000000000,0xfe00000000000000,0xfe0000,0xfe000000,0xfe,0xfe00,0xfe00000000,0xfe0000000000"']
 
     @run_before('run')
     def set_container_variables(self):
@@ -77,20 +76,31 @@ class ds_comm_all_reduce(deepspeed_comm):
         self.container_platform.env_vars = {'NCCL_DEBUG': 'INFO'}
 
 @rfm.simple_test
-class torch_comm_all_reduce(deepspeed_comm):
+class torch_comm_coll_test(deepspeed_comm):
     container_platform = 'Singularity'
-    #container_platform.env_vars = {
-    #        'SINGULARITY_BIND'  :'/var/spool/slurmd,/opt/cray,/usr/lib64/libcxi.so.1,/usr/lib64/libjansson.so.4,/pfs,/scratch,/projappl,/project,/flash,/appl',
-    #    }
-
-    #run_mode   = parameter(['native', 'torchrun'])
+    
+    coll_type  = parameter(['all_reduce', 'all_gather'])
+    run_mode   = parameter(['native', 'torchrun'])
     cont_image = parameter([
         'rocm-6.2.4-python-3.12-pytorch-v2.6.0',
         'rocm-6.2.4-python-3.12-pytorch-v2.7.0',
         'rocm-6.2.4-python-3.12-pytorch-v2.7.1',
     ])
 
-    tags = {'python', 'contrib', 'performance'}
+    tags = {'python', 'performance'}
+
+    @run_before('run')
+    def set_cpu_and_task_binding(self):
+        if self.run_mode == 'native':
+            self.num_tasks = 16
+            self.num_tasks_per_node = 8
+            self.num_gpus_per_node = 8
+            self.job.launcher.options = ['--cpu-bind="mask_cpu:0xfe000000000000,0xfe00000000000000,0xfe0000,0xfe000000,0xfe,0xfe00,0xfe00000000,0xfe0000000000"']
+        if self.run_mode == 'torchrun':
+            self.num_tasks = 2
+            self.num_tasks_per_node = 1
+            self.num_gpus_per_node = 8
+            self.num_cpus_per_task = 56
 
     @run_before('run')
     def set_container_variables(self):
@@ -98,12 +108,17 @@ class torch_comm_all_reduce(deepspeed_comm):
             '/appl/local/containers/sif-images/',
             f'lumi-pytorch-{self.cont_image}.sif',
         )
-        self.container_platform.command = f'bash python-distributed.sh -u communication/all_reduce.py --scan --dist="torch"'
+        py_script = 'communication/' + self.coll_type + '.py --scan --dist="torch"'
+        if self.run_mode == 'native':
+            self.container_platform.command = 'bash python-distributed.sh -u ' + py_script
+        if self.run_mode == 'torchrun':
+            self.container_platform.command = 'bash torch-distributed.sh ' + py_script
         self.env_vars = {
             'NCCL_DEBUG': 'WARN',
             'NCCL_NET_GDR_LEVEL':'PHB',
             'NCCL_SOCKET_IFNAME':'hsn0,hsn1,hsn2,hsn3',
-            'MASTER_ADDR': '$(scontrol show hostnames $SLURM_JOB_NODELIST | head -n 1)',
+            'MASTER_ADDR': '$(scontrol show hostnames ${SLURM_JOB_NODELIST} | head -n 1)',
             'MASTER_PORT':'29500',
             f'WORLD_SIZE': self.num_tasks,
+            'SINGULARITYENV_OMP_NUM_THREADS': '7',
         }
--- checks/microbenchmarks/torch/src/torch-distributed.sh
+++ checks/microbenchmarks/torch/src/torch-distributed.sh
@@ -5,6 +5,9 @@
 # instances via this script using half a node each.
 export RANK=$SLURM_PROCID
 export LOCAL_RANK=$SLURM_LOCALID
+NNODES=$SLURM_NNODES
+NODE_RANK=$SLURM_NODEID
+GPUS_PER_NODE=$SLURM_GPUS_PER_NODE
 
 # This is to workaround AITER issue https://github.com/ROCm/aws-ofi-rccl/issues/16
 export AITER_JIT_DIR=/tmp/my-aiter-jit-dir-$SLURM_LOCALID
@@ -13,5 +16,15 @@ export JIT_WORKSPACE_DIR=$AITER_JIT_DIR
 export CC=clang
 export CXX=clang++
 
+DISTRIBUTED_ARGS="
+    --nproc_per_node $GPUS_PER_NODE \
+    --nnodes $NNODES \
+    --node_rank $NODE_RANK \
+    --master_addr $MASTER_ADDR \
+    --master_port $MASTER_PORT \
+    --rdzv_id=${SLURM_JOB_ID} \
+    --rdzv_backend=c10d \
+    --rdzv_endpoint="${MASTER_ADDR}:${MASTER_PORT}" \
+"
 # Run application
-python "$@"
+torchrun $DISTRIBUTED_ARGS "$@"
