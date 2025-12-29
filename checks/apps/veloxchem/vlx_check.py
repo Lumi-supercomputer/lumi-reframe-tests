b0c4aef77fe9c0f72b8b3ee17301ba2cc48ac486
Maciej Szpindler <m.szpindler@cyfronet.pl>
2025-12-29 16:09:00 +0200

    Split into two test sizes

--- checks/apps/veloxchem/vlx_check.py
+++ checks/apps/veloxchem/vlx_check.py
@@ -1,7 +1,6 @@
 import reframe as rfm
 import reframe.utility.sanity as sn
 
-@rfm.simple_test
 class vlx_check(rfm.RunOnlyRegressionTest):
     modules = ['VeloxChem']
     executable = 'vlx'
@@ -10,30 +9,15 @@ class vlx_check(rfm.RunOnlyRegressionTest):
     valid_prog_environs = ['cpeAMD']
     maintainers = ['mszpindler']
     descr = 'VeloxChem GPU test'
-    time_limit = '15m'
     exclusive_access = True
     keep_files = [output_file]
     
-    energy_reference = -30517.3986504615
-    energy_tolerance = 1.0e-11
-
-    references = {
-        'lumi:gpu': {
-            'time': (1500, None, 0.05, 's')
-        }
-    }
-   
-    @run_after('init')
-    def setup_run(self):
-        job_name = 'g-quad-neutral'
-        input_file = f'{job_name}.inp'
-        self.executable_opts = [input_file, self.output_file]
+    energy_tolerance = 1.0e-12
     
     @run_before('run')
     def setup_job(self):
         self.num_gpus_per_node = 8
         self.num_tasks_per_node = 1
-        self.num_tasks = 8
         self.job.launcher.options = ['--cpus-per-task=56']
         self.env_vars = {
             'SRUN_CPUS_PER_TASK': self.num_gpus_per_node,
@@ -49,9 +33,9 @@ class vlx_check(rfm.RunOnlyRegressionTest):
                 'energy',
                 float
         )
-        energy_diff = sn.abs(energy-self.energy_reference)
+        energy_diff = sn.abs((energy-self.energy_reference)/self.energy_reference)
         return sn.all([
-            sn.assert_found('SCF converged in 11 iterations', self.output_file),
+            sn.assert_found(f'SCF converged in {self.num_iter} iterations', self.output_file),
             sn.assert_found(r'!\s+VeloxChem execution completed', self.output_file),
             sn.assert_lt(energy_diff, self.energy_tolerance)
         ])
@@ -61,3 +45,43 @@ class vlx_check(rfm.RunOnlyRegressionTest):
         return sn.extractsingle(r'!\s+Total execution time is\s+(?P<wtime>\S+)',
                                 self.output_file, 'wtime', float)
 
+
+@rfm.simple_test
+class veloxchem_g_quad(vlx_check):
+    num_tasks = 2
+    time_limit = '26m'
+
+    references = {
+        'lumi:gpu': {
+            'time': (1500, None, 0.05, 's')
+        }
+    }
+
+    energy_reference = -30517.3986504615
+    num_iter = 11
+
+    @run_after('init')
+    def setup_run(self):
+        job_name = 'g-quad-neutral'
+        input_file = f'{job_name}.inp'
+        self.executable_opts = [input_file, self.output_file]
+
+@rfm.simple_test
+class veloxchem_bp_6(vlx_check):
+    num_tasks = 1
+    time_limit = '10m'
+
+    references = {
+        'lumi:gpu': {
+            'time': (575, None, 0.05, 's')
+        }
+    }
+
+    energy_reference = -16618.0888623844
+    num_iter = 6
+
+    @run_after('init')
+    def setup_run(self):
+        job_name = 'bp-6'
+        input_file = f'{job_name}.inp'
+        self.executable_opts = [input_file, self.output_file]
